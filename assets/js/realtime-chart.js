4fa10698d92798e5a7aee9d9a52612a815c31dd1
Suchanapal <122770752+Suchanapal@users.noreply.github.com>
2023-10-30 18:14:43 +0530

    new commit

--- assets/js/realtime-chart.js
+++ assets/js/realtime-chart.js
@@ -56,10 +56,11 @@ setInterval(fetchDataFromEndpoint, 10);
 
 
 
-// chart_script.js doppler 
+
+// chart_script.js doppler let periods = [];
 let periods = [];
 let frequencies = [];
-const maxDataPoints = 15; 
+const maxDataPoints = 15;
 
 let chart = Plotly.newPlot('chart', [{
     x: periods,
@@ -69,19 +70,52 @@ let chart = Plotly.newPlot('chart', [{
     fillcolor: 'rgba(218, 224, 255, 0.8)' 
 }]);
 
-function updateChart() {
-    let newPeriod = new Date();
-    let newFrequency = Math.floor(Math.random() * 100);
-
-    periods.push(newPeriod);
-    frequencies.push(newFrequency);
-
-    if (periods.length > maxDataPoints) {
-        periods.shift(); 
-        frequencies.shift();
+async function fetchData() {
+    try {
+        const response = await fetch('https://team-vigyan.vercel.app/api/v1/doppler');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
     }
-
-    Plotly.update('chart', {x: [periods], y: [frequencies]});
 }
 
-setInterval(updateChart, 200);
+async function updateChartWithApiData() {
+    const apiData = await fetchData();
+
+    if (apiData && Array.isArray(apiData) && apiData.length === 2) {
+        
+        periods = apiData[0];
+        frequencies = apiData[1];
+
+        // Update the chart
+        Plotly.update('chart', {x: [periods], y: [frequencies]});
+    }
+}
+
+updateChartWithApiData();
+setInterval(updateChartWithApiData, 1000);
+
+// --Live footage--
+
+function videoFeed() {
+  // Get the video element
+  const video = document.getElementById("camera-feed");
+
+  // Use navigator.mediaDevices.getUserMedia to access the camera
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      // Set the video source to the camera stream
+      video.srcObject = stream;
+    })
+    .catch(function (error) {
+      console.error("Error accessing camera:", error);
+    });
+}
+
+updateChartWithApiData();
+setInterval(updateChartWithApiData, 1000);
+videoFeed();
+
+
